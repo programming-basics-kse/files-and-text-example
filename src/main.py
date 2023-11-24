52ece5da9e7c745486f0ee001db47550cbec2494
Bardin08 <vbardin810@gmail.com>
2023-11-24 23:16:34 +0200

    Add arguments parsing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-﻿from DataProvider import OlympicsDataProvider
+﻿import argparse
 
-DATASOURCE_PATH = "./data_source.tsv"
-provider = OlympicsDataProvider(DATASOURCE_PATH)
+from DataProvider import OlympicsDataProvider
 
+
+ALLOWED_COUNTRIES = ["UKR", "USA"]
+ALLOWED_YEARS = ["2000", "2004", "2012"]
+
+# ==================== #
+#   data_path          #
+#   -medals AUT 1976   #
+#   -output result.txt #
+# ==================== #
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "data_path",
+    type=str,
+    metavar="data_path")
+
+parser.add_argument(
+    '-medals',
+    required=True,
+    nargs=2,
+    metavar=("country", "year")
+)
+
+args = parser.parse_args()
+print(args)
+
+provider = OlympicsDataProvider(args.data_path)
 print(len(provider.get_applicable_records()))
