bb5a74c825bfa7e1476bab5f12ebb3191f90a248
Bardin08 <vbardin810@gmail.com>
2023-11-26 03:43:45 +0200

    Add stats collecting logic for `overall` command

--- src/StatsCollector.py
+++ src/StatsCollector.py
@@ -64,3 +64,25 @@ class StatsCollector:
             descriptor[record.medal] += 1
 
         return countries
+
+    @staticmethod
+    def collect_overall_stats(provider: OlympicsDataProvider,
+                              countries: [str]) -> [dict[str, dict[str, int]]]:
+        predicate = lambda c: c.noc in countries
+        countries_records = provider.get_applicable_records(predicate)
+
+        stats = {
+            noc: {item.year: 0 for item in countries_records if item.noc == noc}
+            for noc in countries
+        }
+
+        for item in countries_records:
+            if item.noc in countries:
+                stats[item.noc][item.year] += 1
+
+        top_year_stats = {
+            noc: max(year_stats.items(), key=lambda x: x[1], default=(None, 0))
+            for noc, year_stats in stats.items()
+        }
+
+        return top_year_stats
