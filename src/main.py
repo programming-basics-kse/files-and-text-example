258d80c693b1e60389d71e76a1ed035398ca88ec
Bardin08 <vbardin810@gmail.com>
2023-11-30 00:27:15 +0200

    Add args validation

--- src/main.py
+++ src/main.py
@@ -4,24 +4,7 @@ from src.strategy.CommandContext import CommandContext
 from src.strategy.CommandResolver import CommandResolver
 from src.strategy.TotalCommandStrategy import TotalCommandStrategy
 
-ALLOWED_COUNTRIES = ["UKR", "USA"]
-ALLOWED_YEARS = ["2000", "2004", "2012"]
 
-# path = args.data_path
-# if not (os.path.exists(path) and os.path.isfile(path)):
-#     print("Data Source path is not valid!")
-#     exit(1)
-#
-# country, year = args.medals
-# if (country not in ALLOWED_COUNTRIES or
-#         year not in ALLOWED_YEARS):
-#     print("Country or Year invalid!")
-#     exit(1)
-#
-# output_path = args.output
-# if os.path.exists(output_path):
-#     print("Export path is not valid or file already exists!")
-#     exit(1)
 
 args = ArgsProcessor().parse()
 
